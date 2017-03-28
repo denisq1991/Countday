689e203de7858faf7f0aa78ed7db5058e3848375
Denis Quaid <denis.quaid@net-a-porter.com>
2017-03-28 19:43:25 +0100

    Add a navigation item to each VC

--- nv-comms/IconChooserViewController.swift
+++ nv-comms/IconChooserViewController.swift
@@ -9,46 +9,45 @@
 import Foundation
 import UIKit
 
-class IconChooserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class IconChooserViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet var iconChoiceTableView: UITableView?
+    @IBOutlet var iconChoiceCollectionView: UICollectionView?
+    var selectedIcon: UIImage?
     
-    let icons = [("bag", #imageLiteral(resourceName: "bag")),
-                 ("cake", #imageLiteral(resourceName: "cake")),
-                 ("car", #imageLiteral(resourceName: "car")),
-                 ("diamond", #imageLiteral(resourceName: "diamond")),
-                 ("giftbox", #imageLiteral(resourceName: "giftbox")),
-                 ("graduate", #imageLiteral(resourceName: "graduate")),
-                 ("hospital", #imageLiteral(resourceName: "hospital")),
-                 ("music", #imageLiteral(resourceName: "music")),
-                 ("paw", #imageLiteral(resourceName: "paw")),
-                 ("plane", #imageLiteral(resourceName: "plane")),
-                 ("trophy", #imageLiteral(resourceName: "trophy"))] as [(String, UIImage)]
+    let icons = [#imageLiteral(resourceName: "bag"), #imageLiteral(resourceName: "cake"), #imageLiteral(resourceName: "car"), #imageLiteral(resourceName: "diamond"), #imageLiteral(resourceName: "giftbox"), #imageLiteral(resourceName: "graduate"), #imageLiteral(resourceName: "hospital"), #imageLiteral(resourceName: "music"), #imageLiteral(resourceName: "paw"), #imageLiteral(resourceName: "plane"), #imageLiteral(resourceName: "trophy")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.iconChoiceTableView?.separatorColor = UIColor.clear
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "IconChoiceCell", for: indexPath) as! IconChoiceCell
-        let eventType = icons[indexPath.row] as (name: String, image: UIImage)
-        cell.nameLabel?.text = eventType.name
-        cell.iconView?.image = eventType.image
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return icons.count
     }
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 0
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IconChoiceCell", for: indexPath) as! IconChoiceCell
+        cell.iconView?.image = icons[indexPath.row]
+        return cell
     }
+    
+    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = self.iconChoiceCollectionView?.cellForItem(at: indexPath) as? IconChoiceCell else {
+            return
+        }
+        cell.backgroundColor = UIColor.blue
+        self.selectedIcon = cell.iconView?.image
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        guard let cell = self.iconChoiceCollectionView?.cellForItem(at: indexPath) as? IconChoiceCell else {
+            return
+        }
+        cell.backgroundColor = UIColor.clear
+    }
+
 }
 
-class IconChoiceCell: UITableViewCell {
-    @IBOutlet var nameLabel: UILabel?
+class IconChoiceCell: UICollectionViewCell {
     @IBOutlet var iconView: UIImageView?
 }
